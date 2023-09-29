40e8c5ce21e0c6a6e19ad2c0b85832ee2d423ef5
ManuDve <m.alfaro@live.cl>
2023-09-29 14:24:38 -0300

    Implementa servicios get y delete para superheroe

--- src/main/java/com/eureka/superheroestopsecret/service/SuperheroeService.java
+++ src/main/java/com/eureka/superheroestopsecret/service/SuperheroeService.java
@@ -1,4 +1,27 @@
 package com.eureka.superheroestopsecret.service;
 
+import com.eureka.superheroestopsecret.model.Superheroe;
+import com.eureka.superheroestopsecret.repository.SuperheroeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class SuperheroeService {
+    @Autowired
+    private SuperheroeRepository superheroeRepository;
+    public Superheroe createSuperheroe(Superheroe superheroe){
+        return superheroeRepository.save(superheroe);
+    }
+    public Superheroe getSuperheroeById(Long id){
+        Optional<Superheroe> superheroeOptional = superheroeRepository.findById(id);
+        return superheroeOptional.get();
+    }
+    public List<Superheroe> getAllOrganitazions(){
+        return superheroeRepository.findAll();
+    }
+
+    public void deleteSuperheroe(Long id){
+        superheroeRepository.deleteById(id);
+    }
 }
