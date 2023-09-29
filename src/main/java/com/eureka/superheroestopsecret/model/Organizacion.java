42db8362a2afa322f70ce8b0ab243019d1431df3
ManuDve <m.alfaro@live.cl>
2023-09-29 16:40:15 -0300

    Arregla bug de id eliminada en entidad de organizacion

--- src/main/java/com/eureka/superheroestopsecret/model/Organizacion.java
+++ src/main/java/com/eureka/superheroestopsecret/model/Organizacion.java
@@ -12,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 public class Organizacion {
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
