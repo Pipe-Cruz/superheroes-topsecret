c6ae52593602688912d2713c51069c2456cb4d06
ManuDve <m.alfaro@live.cl>
2023-09-29 10:53:59 -0300

    Initial commit

---
+++ src/main/java/com/eureka/superheroestopsecret/SuperheroesTopsecretApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.superheroestopsecret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroesTopsecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheroesTopsecretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eureka/superheroestopsecret/SuperheroesTopsecretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.superheroestopsecret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesTopsecretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
